60d33259aad97dd91a2a9a88694a2bf41de807a1
Greg Mrozek <greg.mrozek@microchip.com>
2025-04-15 16:01:24 -0500

    Clean up ar1100 command print output.

--- mlbb-sam_e51_cnano/src/ball/ar1100_cmd.c
+++ mlbb-sam_e51_cnano/src/ball/ar1100_cmd.c
@@ -16,9 +16,9 @@ void AR1100_CMD_Print_TouchData( void )
 {
     ar1100_touchdata_t touch_data = AR1100_TouchData_Get();
     
-    CMD_PrintString( " T: ", true );
+    CMD_PrintString( "T: ", true );
     CMD_PrintDecimal_U32( (uint32_t)touch_data.down, 0, 0, true );
-    CMD_PrintString( "X: 0x", true );
+    CMD_PrintString( " X: 0x", true );
     CMD_PrintHex_U16( touch_data.x, true );
     CMD_PrintString( " Y: 0x", true );
     CMD_PrintHex_U16( touch_data.y, true );
