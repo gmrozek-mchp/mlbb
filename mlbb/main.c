d4e973bef75a9da994285afaa8b883e2b6b1a1ce
Greg Mrozek <greg.mrozek@microchip.com>
2025-04-23 15:25:32 -0500

    Create new VSCode MPLAB project

---
+++ mlbb/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author greg
+ * @date 2025-04-23
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
